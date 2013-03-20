3bd36ac3802911eed460ace3e0c51d6d4f7a50c0
Kevin Xu <kevin@imkevinxu.com>
2013-03-19 23:00:10 -0700

    added tweet_scraper comment

--- faithinhumanity_app/__init__.py
+++ faithinhumanity_app/__init__.py
@@ -1,4 +1,5 @@
 # Threading the tweet_scraper command in the same process as the web app
+# Avoids having to spawn a separate worker thread to run tweet_scraper
 import threading
 from faithinhumanity_app.management.commands.tweet_scraper import Command as tweet_scraper
 
