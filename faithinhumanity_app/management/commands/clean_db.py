b40b2a033f4aac98b75c6f47c596a3144538dd55
Kevin Xu <kevin@imkevinxu.com>
2013-03-25 09:11:08 -0700

    bullet-proofed clean_db

--- faithinhumanity_app/management/commands/clean_db.py
+++ faithinhumanity_app/management/commands/clean_db.py
@@ -3,7 +3,18 @@ from faithinhumanity_app.models import Tweet
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        total_tweets = Tweet.objects.all()
-        if len(total_tweets) >= 9000:
-            objects_to_keep = Tweet.objects.all()[1000:]
-            Tweet.objects.exclude(pk__in=objects_to_keep).delete()
+        print >> sys.stdout, '\n[Clean DB] Initializing...'
+        try:
+            total_tweets = Tweet.objects.all()
+            if len(total_tweets) >= 9000:
+                objects_to_keep = Tweet.objects.all()[1000:]
+                Tweet.objects.exclude(pk__in=objects_to_keep).delete()
+                print >> sys.stdout, 'DB Successfully Cleaned'
+                print >> sys.stdout, 'From %s rows -> %s rows' % (len(total_tweets), len(Tweet.objects.all()))
+            else:
+                print >> sys.stdout, 'DB Not Cleaned'
+                print >> sys.stdout, 'Currently has only %s rows' % len(total_tweets)
+
+        except Exception, e:
+            print >> sys.stderr, 'Encountered Exception: ', e
+            pass
