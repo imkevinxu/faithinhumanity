4ea61b82f1581c3176782c8736e5f84cda7a1382
Kevin Xu <kevin@imkevinxu.com>
2013-03-25 09:14:03 -0700

    forgot import sys in clean_db

--- faithinhumanity_app/management/commands/clean_db.py
+++ faithinhumanity_app/management/commands/clean_db.py
@@ -1,13 +1,15 @@
 from django.core.management.base import BaseCommand, CommandError
 from faithinhumanity_app.models import Tweet
 
+import sys
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print >> sys.stdout, '\n[Clean DB] Initializing...'
         try:
             total_tweets = Tweet.objects.all()
             if len(total_tweets) >= 9000:
-                objects_to_keep = Tweet.objects.all()[1000:]
+                objects_to_keep = Tweet.objects.all()[10:]
                 Tweet.objects.exclude(pk__in=objects_to_keep).delete()
                 print >> sys.stdout, 'DB Successfully Cleaned'
                 print >> sys.stdout, 'From %s rows -> %s rows' % (len(total_tweets), len(Tweet.objects.all()))
